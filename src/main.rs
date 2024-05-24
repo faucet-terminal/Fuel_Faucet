d99a23db927d0188975904698fbfdf4dc84a9177
panyongxu1002 <panyx1002@gmail.com>
2024-05-24 14:43:38 +0800

    add error handling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-use axum::{extract::Json, routing::get, routing::post, Router};
+use axum::http::StatusCode;
+use axum::response::Json as AxumJson;
+use axum::{extract::Json, response::IntoResponse, routing::get, routing::post, Router};
 use dotenv::dotenv;
-use fuels::accounts::wallet::Wallet;
 use fuels::{crypto::SecretKey, prelude::*};
-use reqwest::Error;
 use serde::{Deserialize, Serialize};
 use std::env;
+use std::num::ParseIntError;
+use std::result::Result as StdResult;
 use std::str::FromStr;
+use thiserror::Error;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct TransferRes {
@@ -20,25 +23,53 @@ struct TransferPost {
     amount: String,
 }
 #[derive(Debug, Serialize, Deserialize)]
-struct Setting {
-    amount: String,
-    frequency: String,
-    alarm: String,
-    chain_type: String,
+struct TransferErrorRes {
+    success: bool,
+    message: String,
+}
+
+#[derive(Debug, Error)]
+enum TransferError {
+    #[error("Network connection error: {0}")]
+    NetworkError(String),
+    #[error("Invalid private key: {0}")]
+    InvalidPrivateKey(String),
+    #[error("Failed to get asset balance: {0}")]
+    GetBalanceError(String),
+    #[error("Invalid amount format: {0}")]
+    InvalidAmountFormat(String),
+    #[error("Invalid receiver address: {0}")]
+    InvalidReceiverAddress(String),
+    #[error("Transaction failed: {0}")]
+    TransactionError(String),
+}
+
+impl IntoResponse for TransferError {
+    fn into_response(self) -> axum::response::Response {
+        let message = self.to_string();
+        let error_res = TransferErrorRes {
+            success: false,
+            message,
+        };
+        let json = AxumJson(error_res);
+        (StatusCode::INTERNAL_SERVER_ERROR, json).into_response()
+    }
 }
 
 #[warn(unused_must_use)]
-async fn transfer(data: Json<TransferPost>) -> Json<TransferRes> {
+async fn transfer(data: Json<TransferPost>) -> StdResult<Json<TransferRes>, TransferError> {
     // Create a provider pointing to the testnet.
-    // This example will not work as the testnet does not support the new version of fuel-core
-    // yet
-
     println!("data: {:#?}", data);
     let key = env::var("KEY").expect("KEY 未设置");
-    let provider = Provider::connect(&data.network).await.unwrap();
+
+    // 网络可能连接失败
+    let provider = Provider::connect(&data.network)
+        .await
+        .map_err(|err| TransferError::NetworkError(err.to_string()))?;
 
     // Setup a private key
-    let secret = SecretKey::from_str(&key).unwrap();
+    let secret = SecretKey::from_str(&key)
+        .map_err(|err| TransferError::InvalidPrivateKey(err.to_string()))?;
 
     // Create the wallet
     let wallet = WalletUnlocked::new_from_private_key(secret, Some(provider));
@@ -46,55 +77,52 @@ async fn transfer(data: Json<TransferPost>) -> Json<TransferRes> {
     // Get the wallet address. Used later with the faucet
     println!("{}", wallet.address().to_string());
 
-    let url = env::var("SETTING_URL").expect("SETTING_URL 未设置");
-    let response = reqwest::get(&url).await.unwrap();
-
-    let setting: Setting = response.json().await.unwrap();
-    println!("setting: {:#?}", setting);
-
     let asset_id: AssetId = BASE_ASSET_ID;
-    let balance: u64 = wallet.get_asset_balance(&asset_id).await.unwrap();
+    let balance = wallet
+        .get_asset_balance(&asset_id)
+        .await
+        .map_err(|err| TransferError::GetBalanceError(err.to_string()))?;
 
     println!("balance: {}, asset_id: {} ", balance, asset_id);
 
-    // const NUM_ASSETS: u64 = 0;
-    let amount: u64 = data.amount.to_owned().parse().unwrap();
-    // let amount: u64 = 10000000000;
+    let amount: u64 = data
+        .amount
+        .to_owned()
+        .parse()
+        .map_err(|err: ParseIntError| TransferError::InvalidAmountFormat(err.to_string()))?;
 
-    // const NUM_COINS: u64 = 1;
-    // let (coins, _) = setup_multiple_assets_coins(wallet.address(), NUM_ASSETS, NUM_COINS, AMOUNT);
-
-    let receiver = Bech32Address::from_str(&data.address).unwrap();
-
-    // let (_tx_id, _receipts) = wallet
-    //     .transfer(&receiver, amount, asset_id, TxPolicies::default())
-    //     .await
-    //     .unwrap();
-
-    let result = wallet
+    let receiver = Bech32Address::from_str(&data.address)
+        .map_err(|err| TransferError::InvalidReceiverAddress(err.to_string()))?;
+    // Send the transaction
+    let (_tx_id, _receipts) = wallet
         .transfer(&receiver, amount, asset_id, TxPolicies::default())
-        .await;
+        .await
+        .map_err(|err: Error| TransferError::TransactionError(err.to_string()))?;
 
-    match result {
-        Ok((_tx_id, _receipts)) => {
-            // 处理成功的结果
-            println!("Transaction successful: {:?}", _tx_id);
-            Json(TransferRes {
-                success: true,
-                tx_id: _tx_id.to_string(),
-                explorer_url: explorer_url(&_tx_id.to_string()),
-            })
-        }
-        Err(e) => {
-            // 处理错误
-            eprintln!("Transaction failed: {:?}", e);
-            Json(TransferRes {
-                success: false,
-                tx_id: String::new(),
-                explorer_url: String::new(),
-            })
-        }
-    }
+    println!("Transaction successful: {:?}", _tx_id);
+
+    Ok(Json(TransferRes {
+        success: true,
+        tx_id: _tx_id.to_string(),
+        explorer_url: explorer_url(&_tx_id.to_string()),
+    }))
+
+    // match result {
+    //     Ok((_tx_id, _receipts)) => {
+    //         // 处理成功的结果
+    //         println!("Transaction successful: {:?}", _tx_id);
+    //         Ok(Json(TransferRes {
+    //             success: true,
+    //             tx_id: _tx_id.to_string(),
+    //             explorer_url: explorer_url(&_tx_id.to_string()),
+    //         }))
+    //     }
+    //     Err(e) => {
+    //         // 处理错误
+    //         eprintln!("Transaction failed: {:?}", e);
+    //         Err(TransferError::TransactionError(e.to_string()))
+    //     }
+    // }
 }
 
 fn explorer_url(tx_id: &str) -> String {
